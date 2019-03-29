f0322ad304ea6b3c245835e5a6a64fa285baa7be
Miguel Campos <amodestduck@gmail.com>
2019-03-29 16:44:04 -0700

    Creating User Interface
    Feature
    User interface drives the boid behaviours.
    -

---
+++ Assets/SliderText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour
+{
+    [SerializeField] Slider slider;
+    [SerializeField] string format;
+    [SerializeField] Text textField;
+
+    public void Awake()
+    {
+        textField.text = string.Format(format, slider.value);
+    }
+
+    public void SetText(float value)
+    {
+        textField.text = string.Format(format, value.ToString("0.00"));
+    }
+}
---
+++ Assets/ToggleText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleText : MonoBehaviour
+{
+    [SerializeField] string on;
+    [SerializeField] string off;
+    [SerializeField] Text textField;
+
+    public void SetText(bool toggle)
+    {
+        if(toggle)
+        {
+            textField.text = on;
+        }
+        else
+        {
+            textField.text = off;
+        }
+    }
+}
